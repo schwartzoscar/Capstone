dae2bbc7d8f069556fd1eb850b82652f34260bcd
O S <ols6vk@umsystem.edu>
2024-02-19 15:16:56 -0600

    Revamped Home page, added base.html and profile,html, reworked style

--- app.py
+++ app.py
@@ -4,14 +4,22 @@ import flask
 
 app = Flask(__name__, static_url_path='', static_folder='.')
 
+#Index app route
 @app.route('/')
 def index():
     pymongo_version = pymongo.__version__
     flask_version = flask.__version__
     return render_template('index.html', pymongo_version=pymongo_version, flask_version=flask_version)
+#Home app route
 @app.route('/home')
 def home():
     return render_template('home.html')
+#Profile app route
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
