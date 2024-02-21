19c01f5f648e90162b242c2ff91e574d091635ac
Jules Parham <parhamjules@gmail.com>
2024-02-20 18:16:10 -0600

    SCRUM-1 working on getting flask backend running with docker

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template
-import pymongo
+from pymongo import MongoClient
 import flask
 
-app = Flask(__name__, static_url_path='', static_folder='.')
+#app = Flask(__name__, static_url_path='', static_folder='.')
+app = Flask(__name__)
+
+
+def get_db():
+    client = MongoClient(
+        host=os.environ['MONGODB_HOST'],
+        username=os.environ['MONGODB_USERNAME'],
+        password=os.environ['MONGODB_PASSWORD'],
+        port=27017,
+        authSource="admin"
+    )
+    db = client["thridderdb"]
+    return db
 
 #Index app route
 @app.route('/')
@@ -10,10 +23,12 @@ def index():
     pymongo_version = pymongo.__version__
     flask_version = flask.__version__
     return render_template('index.html', pymongo_version=pymongo_version, flask_version=flask_version)
+
 #Home app route
 @app.route('/home')
 def home():
     return render_template('home.html')
+
 #Profile app route
 @app.route('/profile')
 def profile():
@@ -22,4 +37,4 @@ def profile():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
