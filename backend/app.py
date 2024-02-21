6e67d68082e2221781bd37d2b71b50403ff35a18
Jules Parham <parhamjules@gmail.com>
2024-02-20 20:17:11 -0600

    SCRUM-1 flask can read and write to mongo db

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import pymongo
 import flask
+import os
 
 #app = Flask(__name__, static_url_path='', static_folder='.')
 app = Flask(__name__)
 
-def get_db():
-    client = pymongo.MongoClient(
-        host=os.environ['MONGODB_HOST'],
-        username=os.environ['MONGODB_USERNAME'],
-        password=os.environ['MONGODB_PASSWORD'],
-        port=27017,
-        authSource="admin"
-    )
-    db = client["thridderdb"]
-    return db
+client = pymongo.MongoClient(
+    host=os.environ['MONGODB_HOST'],
+    username=os.environ['MONGODB_USERNAME'],
+    password=os.environ['MONGODB_PASSWORD'],
+    port=27017,
+    authSource="admin"
+)
+db = client["thridderdb"]
 
 #Index app route
 @app.route('/')
@@ -33,7 +32,19 @@ def home():
 def profile():
     return render_template('profile.html')
 
+#Test write to DB
+@app.route('/store_animals')
+def store_animals():
+    db["animal_tb"].drop()
+    id = db["animal_tb"].insert_one({"name": "Turtle", "type": "wild"})
+    return jsonify({"success": "true"})
 
+# Test read from DB
+@app.route('/get_animals')
+def get_stored_animals():
+    _animals = db.animal_tb.find()
+    animals = [{"name": animal["name"], "type": animal["type"]} for animal in _animals]
+    return jsonify({"animals": animals})
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
