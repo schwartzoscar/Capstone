f989253cfad0445350b4d9e15f45b7232f61a1d4
Jules Parham <parhamjules@gmail.com>
2024-02-20 19:33:46 -0600

    SCRUM-1 flask now running in docker

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,6 @@ import flask
 #app = Flask(__name__, static_url_path='', static_folder='.')
 app = Flask(__name__)
 
-
 def get_db():
     client = pymongo.MongoClient(
         host=os.environ['MONGODB_HOST'],
@@ -37,4 +36,4 @@ def profile():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
