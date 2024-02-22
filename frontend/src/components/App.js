139005496883103d8f6c54ec902c339c10dabb7c
Jules Parham <parhamjules@gmail.com>
2024-02-21 20:00:41 -0600

    SCRUM-2 installed libraries, file structure

---
+++ frontend/src/components/App.js
@@ -0,0 +1,10 @@
+export default function App() {
+  return(
+    <div>
+      <header>
+        <p>React App</p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+      </header>
+    </div>
+  );
+}
