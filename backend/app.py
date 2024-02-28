4328319d3ba6f4ccc2db0d4da4d82b0414f95fd0
Oscar S <ol6vk@umsystem.edu>
2024-02-28 17:46:08 -0600

    Database added

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import pymongo
 import flask
 import os
@@ -13,14 +13,17 @@ client = pymongo.MongoClient(
     port=27017,
     authSource="admin"
 )
-db = client["thridderdb"]
+
+
+db = client['thritterdb']
+users_collection = db['users']
+
 
 #Index app route
 @app.route('/')
 def index():
-    pymongo_version = pymongo.__version__
-    flask_version = flask.__version__
-    return render_template('index.html', pymongo_version=pymongo_version, flask_version=flask_version)
+    accounts = list(users_collection.find({}, {'_id': 0}))
+    return render_template('index.html',accounts=accounts)
 
 #Home app route
 @app.route('/home')
@@ -32,19 +35,43 @@ def home():
 def profile():
     return render_template('profile.html')
 
-#Test write to DB
-@app.route('/store_animals')
-def store_animals():
-    db["animal_tb"].drop()
-    id = db["animal_tb"].insert_one({"name": "Turtle", "type": "wild"})
-    return jsonify({"success": "true"})
+@app.route('/register', methods=['POST','GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-# Test read from DB
-@app.route('/get_animals')
-def get_stored_animals():
-    _animals = db.animal_tb.find()
-    animals = [{"name": animal["name"], "type": animal["type"]} for animal in _animals]
-    return jsonify({"animals": animals})
+        last_user = users_collection.find_one({}, sort=[('_id', pymongo.DESCENDING)])
+        if last_user:
+            user_id = last_user.get('user_id', 0) + 1
+        else:
+            user_id = 1
+            
+        users_collection.insert_one({'user_id': user_id, 'username': username, 'email': email, 'password': password})
+        accounts = list(users_collection.find({}, {'_id': 0})) 
+        return render_template('register.html', accounts=accounts)
+    elif request.method == 'GET':
+        return render_template('register.html')
 
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = users_collection.find_one({'username': username, 'password': password})
+        if user:
+            return render_template('login.html', message='Login successful', username=username, email=user.get('email', ''), password=user.get('password', ''))
+        else:
+             return render_template('login.html', message='Username/Password Incorrect')
+    elif request.method == 'GET':
+        return render_template('login.html')
+
+@app.route('/cleardb')
+def cleardb():
+    users_collection.delete_many({})
+    
+    return render_template('cleardb.html')
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
