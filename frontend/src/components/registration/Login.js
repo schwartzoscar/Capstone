8082692addc463cf487cbd7df57a79a65f5f056c
Jules Parham <parhamjules@gmail.com>
2024-03-02 17:18:47 -0600

    SCRUM-17 refactored login form to use new form components

--- frontend/src/components/registration/Login.js
+++ frontend/src/components/registration/Login.js
@@ -1,72 +1,38 @@
-import { useState } from 'react';
-import { Link } from "react-router-dom";
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useForm, FormProvider } from "react-hook-form";
+import { apiClient } from "../../helpers/requestHelpers";
+import Button from "../elements/Button";
+import { TextField, SubmitButton } from "../elements/FormField";
 
-const Login = () => {
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const navigate = useNavigate();
+export default function Login() {
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const form = useForm();
 
-        axios.post( 'http://localhost:3000/login', {email, password})
-        .then(result => {
-            console.log(result);
-            if(result.data === "Success"){
-                console.log("Login Success");
-                alert('Login successful!')
-                navigate('/home');
-            }
-            else{
-                alert('Incorrect password! Please try again.');
-            }
-        })
-        .catch(err => console.log(err));
-    }
+  const handleSubmit = data => {
+    apiClient.post( '/login', data)
+      .then(result => {
+        if(result.data === "Success"){
+          alert('Login successful!')
+        } else {
+          alert('Incorrect password! Please try again.');
+        }
+      })
+      .catch(err => console.log(err));
+  }
 
-
-    return (
+  return(
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="bg-white p-3 rounded" style={{width : '40%'}}>
+        <h2 className='mb-3'>Login</h2>
         <div>
-            <div className="d-flex justify-content-center align-items-center text-center vh-100">
-                <div className="bg-white p-3 rounded" style={{width : '40%'}}>
-                    <h2 className='mb-3'>Login</h2>
-                    <form onSubmit={handleSubmit}>
-                        <div className="mb-3 text-start">
-                            <label htmlFor="exampleInputEmail1" className="form-label">
-                                <strong>Email ID</strong>
-                            </label>
-                            <input 
-                                type="email" 
-                                placeholder="Enter Email"
-                                className="form-control" 
-                                id="exampleInputEmail1" 
-                                onChange={(event) => setEmail(event.target.value)}
-                                required
-                            /> 
-                        </div>
-                        <div className="mb-3 text-start">
-                            <label htmlFor="exampleInputPassword1" className="form-label">
-                                <strong>Password</strong>
-                            </label>
-                            <input 
-                                type="password" 
-                                placeholder="Enter Password"
-                                className="form-control" 
-                                id="exampleInputPassword1" 
-                                onChange={(event) => setPassword(event.target.value)}
-                                required
-                            />
-                        </div>
-                        <button type="submit" className="btn btn-primary">Login</button>
-                    </form>
-                    <p className='container my-2'>Don&apos;t have an account?</p>
-                    <Link to='/register' className="btn btn-secondary">Register</Link>
-                </div>
-            </div>
+          <FormProvider {...form}>
+            <TextField name="email" label="Email" validation={{ required: "Email is required." }}/>
+            <TextField name="password" label="Password" validation={{ required: "Password is required." }}/>
+            <SubmitButton className="btn-primary" onClick={handleSubmit}>Login</SubmitButton>
+          </FormProvider>
         </div>
-    )
+        <p className='container my-2'>Don&apos;t have an account?</p>
+        <Button to='/register' className="btn-secondary">Register</Button>
+      </div>
+    </div>
+  )
 }
-
-export default Login;
