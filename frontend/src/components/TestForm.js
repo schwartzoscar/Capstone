5ea771e5938641fd1b971067650b4271349c3589
Jules Parham <parhamjules@gmail.com>
2024-03-02 06:56:12 -0600

    SCRUM-5 setup routing and starting files for home page and profile page

--- frontend/src/components/TestForm.js
+++ frontend/src/components/TestForm.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useForm, FormProvider } from 'react-hook-form';
 import { SubmitButton, TextField } from "./elements/FormField";
+import Button from "./elements/Button";
 
 export default function TestForm() {
 
@@ -16,15 +17,18 @@ export default function TestForm() {
   }
 
   return(
-    <div style={{margin: 50, padding: 10, border: "0.5px solid #202020"}}>
-      <h1>Test Form</h1>
-      <FormProvider {...form}>
-        <TextField name="name" label="Name"/>
-        <TextField name="email" label="Email" validation={{ required: "Email is required." }}/>
-        <TextField name="password" label="Password" validation={{ required: "Password is required." }}/>
-        <TextField name="confirmPass" label="Confirm Password"/>
-        <SubmitButton onClick={submit} loading={loading} className="btn-primary" icon="fa-user"/>
-      </FormProvider>
+    <div>
+      <div style={{margin: 50, padding: 10, border: "0.5px solid #202020"}}>
+        <h1>Test Form</h1>
+        <FormProvider {...form}>
+          <TextField name="name" label="Name"/>
+          <TextField name="email" label="Email" validation={{ required: "Email is required." }}/>
+          <TextField name="password" label="Password" validation={{ required: "Password is required." }}/>
+          <TextField name="confirmPass" label="Confirm Password"/>
+          <SubmitButton onClick={submit} loading={loading} className="btn-primary" icon="fa-user"/>
+        </FormProvider>
+      </div>
+      <Button to="/" className="btn-secondary">Home</Button>
     </div>
   );
 }
---
+++ frontend/src/components/home/Home.js
@@ -0,0 +1,11 @@
+import Button from "../elements/Button";
+
+export default function Home() {
+  return(
+    <div>
+      <h1>Home Page</h1>
+      <Button to="/profile" className="btn-primary">My Profile</Button>
+      <Button to="/test" className="btn-secondary">Test Form</Button>
+    </div>
+  );
+}
