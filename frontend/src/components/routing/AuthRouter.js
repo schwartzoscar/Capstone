51f2a7b0247c9aa024fd63bca07c211bc66788ad
Jules Parham <parhamjules@gmail.com>
2024-03-02 21:28:05 -0600

    SCRUM-17 deleted test form

--- frontend/src/components/routing/AuthRouter.js
+++ frontend/src/components/routing/AuthRouter.js
@@ -1,12 +1,10 @@
 import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import Home from "../home/Home";
 import Profile from "../profile/Profile";
-import TestForm from "../TestForm";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home/> },
   { path: "/profile", element: <Profile/> },
-  { path: "/test", element: <TestForm/> },
   { path: "*", element: <Navigate to="/" replace /> }
 ]);
 
