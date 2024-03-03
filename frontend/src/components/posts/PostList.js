5510ad3f3e325598186689952ca1a140f3b80e92
Jules Parham <parhamjules@gmail.com>
2024-03-03 13:11:01 -0600

    SCRUM-16 implementing infinite scroll

--- frontend/src/components/posts/PostList.js
+++ frontend/src/components/posts/PostList.js
@@ -1,22 +1,49 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { apiClient } from "../../helpers/requestHelpers";
 import { handleResp } from "../../helpers/responseHelpers";
 import { useAuthContext } from "../../contexts/AuthContext";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import PostListItem from "./PostListItem";
 
 export default function PostList() {
 
   const [{ currentUser }] = useAuthContext();
   const [posts, setPosts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const limit = 25;
 
-  useEffect(() => {
-    apiClient.post('/posts/list', {userId: currentUser.id}).then(resp => {
-      handleResp(resp, data => setPosts(data.posts));
+  const getPosts = async(nextPage = 1) => {
+    const post = {userId: currentUser.id, page: nextPage, limit};
+    const resp = await apiClient.post('/posts/list', post);
+    handleResp(resp, data => {
+      setPosts(data.posts);
+      setTotal(data.total);
     });
-  }, [currentUser]);
+    setPage(nextPage);
+  }
+
+  useEffect(getPosts, [currentUser]);
+
+  const items = useMemo(() => posts.map(post => <PostListItem post={post}/>), [posts]);
 
   return(
     <div>
-
+      <InfiniteScroll
+        dataLength={posts.length}
+        next={() => getPosts(page + 1)}
+        hasMore={total > page * limit}
+        loader={<h4>Loading...</h4>}
+        endMessage={<p className="text-center fw-bold">Yay! You have seen it all</p>}
+        // below props only if you need pull down functionality
+        refreshFunction={getPosts}
+        pullDownToRefresh
+        pullDownToRefreshThreshold={50}
+        pullDownToRefreshContent={<h3 className="text-center">&#8595; Pull down to refresh</h3>}
+        releaseToRefreshContent={<h3 className="text-center">&#8593; Release to refresh</h3>}
+      >
+        {items}
+      </InfiniteScroll>
     </div>
   );
 }
---
+++ frontend/src/components/posts/PostListItem.js
@@ -0,0 +1,7 @@
+export default function PostListItem({ post }) {
+  return(
+    <div className="post-list-item">
+      <p>post list item</p>
+    </div>
+  );
+}
