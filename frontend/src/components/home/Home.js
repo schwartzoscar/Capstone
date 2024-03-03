fb159fb7ca8239ab4cbb60217d65dad98efbffc3
Jules Parham <parhamjules@gmail.com>
2024-03-03 09:26:22 -0600

    SCRUM-16 installed library for infinite scroll

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,9 +1,11 @@
 import Button from "../elements/Button";
+import PostList from "../posts/PostList";
 
 export default function Home() {
   return(
     <div>
       <h1>Home Page</h1>
+      <PostList/>
       <Button to="/profile" className="btn-primary">My Profile</Button>
       <Button to="/test" className="btn-secondary">Test Form</Button>
     </div>
