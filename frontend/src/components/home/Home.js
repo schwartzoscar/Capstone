c9663a5779edf3217d0835c5da4014ae997c3f39
Jules Parham <parhamjules@gmail.com>
2024-03-16 19:24:57 -0500

    SCRUM-16 moved temp homepage to separate file

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,25 +1,11 @@
-import Base from '../../components/base/Base';
-import PostList from "../posts/PostList";
+import Button from "../elements/Button";
 
 export default function Home() {
-  return (
-    <div className="page-container">
+  return(
+    <div>
       <h1>Home Page</h1>
-       <Base>
-         <div className="d-flex g-20 mt-20">
-           <div className="page-section" style={{width: 400}}>
-             <p>left panel</p>
-           </div>
-           <div className="flex-grow-1 page-section">
-             <h3>Posts</h3>
-             <hr/>
-             <PostList/>
-           </div>
-           <div className="page-section" style={{width: 400}}>
-             <p>right panel</p>
-           </div>
-         </div>
-       </Base>
+      <Button to="/profile" className="btn-primary">My Profile</Button>
+      <Button to="/test" className="btn-secondary">Test Form</Button>
     </div>
   );
 }
