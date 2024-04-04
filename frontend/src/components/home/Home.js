5a08c9289dd2ba160b8b527c6ecdb99045d26abf
Oscar S <ol6vk@umsystem.edu>
2024-04-04 15:53:08 -0500

    Created home.css file instead of using reset.css (temp)

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,15 +1,21 @@
+
 import Base from "../base/Base";
 import PostList from "../posts/PostList";
+import Button from "../elements/Button";
+import InfiniteScroll from "../elements/InfiniteScroll";
+import "../../styles/0_base/home.css"
 
 export default function HomeTemp() {
   return (
     <div className="page-container">
-      <h1>Home Page</h1>
+      <h1>Welcome To Thridder</h1>
       <Base>
         <div className="d-flex g-20 mt-20">
-          <div className="page-section" style={{width: 400}}>
-            <p>left panel</p>
-          </div>
+        <div className="page-section" style={{width: 200, textAlign: 'center'}}>
+          <Button to="/profile" className="btn-home">My Profile</Button>
+          <br />
+          <Button to="/home" className="btn-home">Home</Button>
+        </div>
           <div className="flex-grow-1 page-section">
             <h3>Posts</h3>
             <hr/>
