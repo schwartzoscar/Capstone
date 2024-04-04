4293ae13a6130374caf48c91478708c73ef3f6eb
Oscar S <ol6vk@umsystem.edu>
2024-04-04 16:28:59 -0500

    Starting to add animations to page

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -3,18 +3,19 @@ import Base from "../base/Base";
 import PostList from "../posts/PostList";
 import Button from "../elements/Button";
 import InfiniteScroll from "../elements/InfiniteScroll";
-import "../../styles/0_base/home.css"
-
-export default function HomeTemp() {
+import "../../styles/4_home/home.css"
+export default function Home() {
   return (
     <div className="page-container">
       <h1>Welcome To Thridder</h1>
       <Base>
         <div className="d-flex g-20 mt-20">
-        <div className="page-section" style={{width: 200, textAlign: 'center'}}>
-          <Button to="/profile" className="btn-home">My Profile</Button>
-          <br />
-          <Button to="/home" className="btn-home">Home</Button>
+        <div className="page-section" style={{width: 200, height: '75vh', textAlign: 'center'}}>
+          <hr/>
+            <Button to="/profile" className="btn-home">My Profile</Button>
+          <hr/>
+            <Button to="/home" className="btn-home">Home</Button>
+          <hr/>
         </div>
           <div className="flex-grow-1 page-section">
             <h3>Posts</h3>
