550b346ebaeb47176ee98c48a24b134b636e854f
Jules Parham <parhamjules@gmail.com>
2024-04-15 20:21:18 -0500

    SCRUM-36 deleted hometemp

--- frontend/src/components/routing/AuthRouter.js
+++ frontend/src/components/routing/AuthRouter.js
@@ -1,11 +1,9 @@
 import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import Home from "../home/Home";
 import Profile from "../profile/Profile";
-import HomeTemp from "../home/HomeTemp";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home/> },
-  { path: "/temp", element: <HomeTemp/> },
   { path: "/profile", element: <Profile/>, children: [
     { path: ":userId", element: <Profile/> }
   ]},
