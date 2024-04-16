d1595eecfa42682357fc6608c70a1c083c25d44e
Oscar S <ol6vk@umsystem.edu>
2024-04-16 14:48:18 -0500

    Seperation of Privacy & Content policy

--- frontend/src/components/elements/ContentModal.js
+++ frontend/src/components/elements/ContentModal.js
@@ -6,7 +6,24 @@ import Button from "./Button";
 export default function ContentModal(props) {
   return (
     <Modal2 show={props.show} setShow={props.setShow}>
-        <h5></h5>
+        <h5>General Guidelines
+            Respectful Communication: All users must engage respectfully with others, avoiding language that is harmful, threatening, or offensive. Harassment, bullying, and hate speech are strictly prohibited.
+
+            Authenticity: We encourage users to share content that is genuine and original. Impersonation, misleading information, or any form of intellectual property theft will not be tolerated.
+
+            Safety: Content promoting dangerous or illegal activities, including but not limited to physical harm, fraud, or illegal substance abuse, is forbidden.
+
+            Privacy: Users must respect the privacy of others and should not share personal information about someone without their explicit consent.
+
+            Intellectual Property: Users should only post content for which they own the rights, or for which they have obtained necessary permissions. Copyrighted material must not be used without authorization from the copyright owner.
+
+            Content Restrictions
+            No posting of copyrighted material unless you have the legal right to do so.
+            No posting of content that involves nudity, sexual acts, or exploits minors in any way.
+            No posting of content that promotes or incites violence against individuals or groups based on race, ethnicity, religion, gender, sexual preference, disability, or any other characteristic.
+            No posting of content that supports or glorifies hate groups.
+            Enforcement and Reporting
+            We reserve the right to remove any content that violates these guidelines and to suspend or terminate accounts involved in severe or repeated offenses.</h5>
     </Modal2>
   );
 }
--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -3,12 +3,20 @@ import Base from "../base/Base";
 import PostList from "../posts/PostList";
 import Button from "../elements/Button";
 import ContentModal from "../elements/ContentModal";
+import PrivacyModal from "../elements/PrivacyModal";
 
 export default function Home() {
-  const [showContentModal, setShowModal] = useState(false);
 
-  const handleModalButtonClick = () => {
-    setShowModal(true);
+  const [showContentModal, setContentShowModal] = useState(false);
+
+  const handleContentModalButtonClick = () => {
+    setContentShowModal(true);
+  };
+  
+    const [showPrivacyModal, setPrivacyShowModal] = useState(false);
+
+    const handlePrivacyModalButtonClick = () => {
+      setPrivacyShowModal(true);
   };
 
   return (
@@ -31,11 +39,11 @@ export default function Home() {
             <hr />
             <Button to="/news" className="btn-home">NEWS</Button>
             <div style={{ height: '73%'}}></div>
-            <Button onClick={handleModalButtonClick} className="btn-home-solid">CONTENT POLICY</Button>
-            <ContentModal show={showModal} setShow={setShowModal} />
+            <Button onClick={handleContentModalButtonClick} className="btn-home-solid">CONTENT POLICY</Button>
+            <ContentModal show={showContentModal} setShow={setContentShowModal} />
             <hr />
-            <Button onClick={handleModalButtonClick} className="btn-home-solid">CONTENT POLICY</Button>
-            <ContentModal show={showModal} setShow={setShowModal} />
+            <Button onClick={handlePrivacyModalButtonClick} className="btn-home-solid">PRIVACY POLICY</Button>
+            <PrivacyModal show={showPrivacyModal} setShow={setPrivacyShowModal} />
           </div>
           <div className="flex-grow-1 page-section">
             <h3>POSTS</h3>
