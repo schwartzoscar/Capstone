75791fe8a1db87b7b0e8f22de2fe5c5ae11e3b32
Jules Parham <parhamjules@gmail.com>
2024-04-18 17:12:00 -0500

    SCRUM-27 fixed routing

--- frontend/src/components/routing/AuthRouter.js
+++ frontend/src/components/routing/AuthRouter.js
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom"
 import Home from "../home/Home";
 import Profile from "../profile/Profile";
 import HomeTemp from "../home/HomeTemp";
+import BlogCreation from "../blog/BlogCreation";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home/> },
   { path: "/temp", element: <HomeTemp/> },
   { path: "/profile", element: <Profile/> },
+  { path: "/create-blog", element: <BlogCreation/> },
   { path: "*", element: <Navigate to="/" replace /> }
 ]);
 
